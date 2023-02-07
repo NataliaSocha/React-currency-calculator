77d5a31a06e8d122641709ae95d2d4b0f7102fa4
Socha89Natalia <112352063+Socha89Natalia@users.noreply.github.com>
2023-02-07 09:58:57 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Header from "./Components/Header";
 import FormCurrency from "./Components/FormCurrency";
-import Preloader from "./Components/Preloader/Loader";
-import ResultAndAddPositiveValueOfTheNumber from "./Components/Currencies/ResultAndaddPositiveValueOfTheNumber";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <FormCurrency />
-      <Preloader />
-      <ResultAndAddPositiveValueOfTheNumber />
     </div>
   );
 }
